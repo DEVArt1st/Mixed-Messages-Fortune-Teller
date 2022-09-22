e0492ebb2af9cd562a51ab54352ffb555912b7c8
Isaiah Brown <irbrown54321@gmail.com>
2022-09-21 23:31:58 -0400

    Completed style and html connects along with JS code changes for html and connect

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,5 +1,3 @@
-// var favoriteNumber = document.getElementById("numberInputForm").elements.namedItem("numberInput").value;
-
 function oddOrEven(num){
     const odd = "odd";
     const even = "even";
@@ -30,36 +28,47 @@ function pickRandomFortuneNumber(num){
 
 function tellFortune(num){
     let randomNum = pickRandomFortuneNumber(num);
-
+    let text = document.getElementById("paragraph");
     switch(randomNum){
         case 1:
-            console.log("You will got on a trip!");
+            text.innerHTML = "You will got on a trip!";
             break;
         case 2:
-            console.log("You will be rich!");
+            text.innerHTML = "You will be rich!";
             break;
         case 3:
-            console.log("You will get an A on a test!");
+            text.innerHTML = "You will get an A on a test!";
             break;
         case 4:
-            console.log("Good fortune will be yours.");
+            text.innerHTML = "Good fortune will be yours.";
             break;
         case 5:
-            console.log("Do a good deed today.");
+            text.innerHTML = "Do a good deed today.";
             break;
         case 6:
-            console.log("Someone will call you today.");
+            text.innerHTML = "Someone will call you today.";
             break;
         case 7:
-            console.log("You will go to a party soon!");
+            text.innerHTML = "You will go to a party soon!";
             break;
         case 8:
-            console.log("Be careful on Tuesday.");
+            text.innerHTML = "Be careful on Tuesday.";
             break;
         default:
-            console.log("Please enter valid favorite number.");
+            text.innerHTML = "Please enter valid favorite number.";
             break;
     }
 }
 
-console.log(oddOrEven(3));
+function btnFucntion(){
+    let numInput = document.getElementById("numberInput").value;
+
+    if(numInput === ""){
+        numInput = document.getElementById("numberInput").value = "NaN";
+    }
+    else{
+        numInput = parseInt(document.getElementById("numberInput").value);
+    }
+
+    tellFortune(numInput);
+}
